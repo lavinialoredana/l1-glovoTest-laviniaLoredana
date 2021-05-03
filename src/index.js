13445c51b5df383f540345ae0486a7d49c38a19f
Lavinia Loredana <lavinialoreda@gmail.com>
2021-05-03 14:17:34 +0200

    fetching data from api displaying drinks data object starting with G

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, Glovo!')
+const fetch = require("node-fetch");
+
+//  punto 2 - changed the value of the first letter from the given fetch link from a to g
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g')
+.then(response => response.json())
+.then(data => {
+        console.log(" this is my drinks object starting with G", data);
+})
